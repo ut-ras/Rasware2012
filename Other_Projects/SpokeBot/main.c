41fcb0be60109a26bbfdf7b99536702ed3363eed
Christopher Haster <chaster@utexas.edu>
2013-02-20 00:11:32 -0600

    Fixed weird issue where Windows thought two folders were the same because of a different case.

--- Other_Projects/SpokeBot/main.c
+++ Other_Projects/SpokeBot/main.c
@@ -1,55 +1,65 @@
 #include "inc/hw_types.h"		// tBoolean
-#include "RASDemo.h"
 #include "utils/uartstdio.h"	// input/output over UART
 #include "driverlib/uart.h"		// input/output over UART
 #include "RASLib/init.h"
 
-int main(void)
-{	
-	char ch;	  	 
+#include "comm.h"
+#include "motor.h"
+#include "crane.h"
+
+// The main function simply initializes all of the 
+// devices that will be used. Then it enters a loop
+// that checks comm for incomming data over UART to 
+// respond to
+
+// Most message over UART should be a command and value pair:
+// 	m - set left motor
+// 	n - set right motor
+// 	c - sets the crane state
+// 	p - ping the rasboard, responds with c
+// 	r - reads the value sensor (will be implemented if needed)
+//  ! - reset everything
+// Responses can be:
+// 	c - success
+//  r_ - successful read with data
+//  x - failure
+
+int main(void) {	  	 
 	LockoutProtection();
 	InitializeMCU();
-	initUART();																							    
 	
-	while(1) {	
-		UARTprintf("\nRAS Demo for Robotathon 2012\n");
-		UARTprintf("  0=UART Demo\n  1=Motor Demo\n");
-		UARTprintf("  2=Servo Demo\n  3=Line Sensor\n");
-		UARTprintf("  4=IR Sensor Demo\n  5=Encoders Demo\n");
+	comm_init();
+	motor_init();
+	crane_init();
+	
+	while (1) {
+		unsigned char cmd = comm_read();
 		
-		UARTprintf(">> ");
-		ch = getc();
-		putc(ch);
-		UARTprintf("\n");
-
-		if (ch == '0') {
-			UARTprintf("\nUART Demo\n");
-			uartDemo();	 
-		}
-		else if (ch == '1') {
-			UARTprintf("\nMotor Demo\n");
-			initMotors();
-			motorDemo(); 
-		}
-		else if (ch == '2') {
-			UARTprintf("\nServo Demo\n");
-			initServo();
-			servoDemo();   
-		}
-		else if (ch == '3') {			   
-			UARTprintf("\nLine Sensor Demo\n");
-			initLineSensor();		  
-			lineSensorDemo();
-		}
-		else if (ch == '4') {	   
-			UARTprintf("\nIR Sensor Demo\n");
-			initIRSensor();
-			IRSensorDemo();	 
-		}
-		else if (ch == '5') {
-			UARTprintf("\nEncoders Demo\n");
-			initEncoders();
-			encoderDemo();
+		switch (cmd) {
+			case 'm': // left motor
+				motor_left(comm_read());
+				comm_write('c');
+				break;
+			case 'n': // right motor
+				motor_right(comm_read());
+				comm_write('c');
+				break;
+			case 'c': // crane
+				crane_set(comm_read());
+				comm_write('c');
+				break;
+			case 'p': //ping
+				comm_write('c');
+				break;
+			case '!': //
+				motor_left(0);
+				motor_right(0);
+				crane_set(0);
+			  comm_write('c');
+				break;
+			default:
+				comm_write('x');
+				break;
 		}
 	}
 }
