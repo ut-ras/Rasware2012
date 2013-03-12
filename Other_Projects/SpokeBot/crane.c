ac97633d9f620f5505728b46465291c4ce1e0944
Spokebot <spokebot@spokebot.(none)>
2013-03-12 01:58:49 -0500

    added crane state test for disk pick-up -Kevin

--- Other_Projects/SpokeBot/crane.c
+++ Other_Projects/SpokeBot/crane.c
@@ -15,4 +15,34 @@ void crane_set(unsigned char val) {
 	SetServoPosition(SERVO_0, val);
 }
 
+//*******************************************************
+//  Initiliazes pin for signal test on crane to
+//      determine state of disk pick-up
+//*******************************************************
+void isDisk_grabbedInit(void) {
+    //Pin initilizations
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
+    GPIOPinTypeGPIOInput(GPIO_PORTB_BASE, GPIO_PIN_2);
+}
 
+
+//********************************************************
+//  Checks input signal from PB-2, if signal high,
+//      then disk is in contact with crane
+//
+//  Inputs: void
+//  Outputs: tbool TRUE if disk connected, tbool FALSE if not
+//
+//  -->Uses PortB Pin2, call isDisk_grabbedInit before
+//      calling this function
+//********************************************************
+tbool isDisk_grabbed(void) {
+    //Test Input
+    if(GPIOPinRead(GPIO_PORTB_BASE, GPIO_PIN_2) != 0) {
+        // Pin High
+        return true;
+    } else {
+        // Pin Low
+        return false;
+    }
+}
