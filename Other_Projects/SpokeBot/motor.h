6f2172e82c4042a4293d98839f4cfd7181ed2fa0
RachSmith <RachSmith@utexas.edu>
2013-04-03 18:15:00 -0500

    Update motor.h

--- Other_Projects/SpokeBot/motor.h
+++ Other_Projects/SpokeBot/motor.h
@@ -13,11 +13,14 @@ void motor_right(signed char val);
 // Stop the motors
 void motor_stop(void);
 
-// Turn by given degrees
-void motor_turn(signed char degrees);
+// Turn by given degrees ccw
+void motor_turn(short degrees);
 
 // Drive forward by distance given in inches
 // Make sure to compensate for motor drift here
 void motor_forward(unsigned char distance);
+
+// Same as motor_forward but back
+void motor_backward(unsigned char distance);
 
 #endif
