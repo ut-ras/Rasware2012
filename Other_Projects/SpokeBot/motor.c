c6a29746916e768d0d0200deb3f37aa36207383c
RachSmith <RachSmith@utexas.edu>
2013-04-03 18:11:59 -0500

    Update motor.c

--- Other_Projects/SpokeBot/motor.c
+++ Other_Projects/SpokeBot/motor.c
@@ -9,8 +9,16 @@
 
 #include "motor.h"
 
+#define TICKS_DEGREE 5
+#define ERR 5
+#define kp_L 0.83
+#define kp_R 0.83
+#define DIST 0.00166
+#define SCALE 0
+
+
 void motor_init(void) {
-	InitializeMotors(false, false);
+	InitializeMotors(false), false);
 }
 
 void motor_left(unsigned char val) {
@@ -26,8 +34,36 @@ void motor_stop(void) {
 	SetMotorPower(MOTOR_1, 0);
 }
 
-void motor_turn(unsigned char degrees) {
-	// TODO for rachel
+void motor_turn(short degrees) {
+	
+	int encoder0;
+	int encoder1; 
+	int speed_L = 0;
+	int speed_R = 0;
+	int pos = degree * TICKS_DEGREE;
+	PresetEncoderCounts(0, 0);
+	InitializeEncoders(true, false);
+	encoder0 = GetEncoderCount(ENCODER_0);
+	encoder1 = GetEncoderCount(ENCODER_1);
+	while((encoder0) > (pos+ERR) || (encoder0< pos-ERR)){ 
+		    if(encoder0 > (pos+ERR)){
+			   speed_L = -(100+SCALE);
+			   speed_R = -(speed_L);
+			}
+
+		    else if(encoder0 < (pos-ERR)) {
+			   speed_L = 100;
+	     		   speed_R = -(speed_L+SCALE);
+		    }
+		    SetMotorPowers(speed_L,speed_R); 
+		    //UARTprintf("Left Motor = %d, Right Motor = %d\r\n", speed_L, speed_R);
+	            //UARTprintf("encoder0 = %d, encoder1 = %d \r\n", encoder0, encoder1); 
+		    encoder0 = GetEncoderCount(ENCODER_0);
+	            encoder1 = GetEncoderCount(ENCODER_1);									   
+	}	
+        SetMotorPowers(0,0);
+  	//UARTprintf("Done\r\n");
+	//Wait(10000);
 }
 
 
